17bc1d3a43512a6190afa7187312a8dcb862140e
edgararaos <32717985+edgararaos@users.noreply.github.com>
2017-10-11 16:22:08 -0500

    Initial commit

---
+++ app/src/main/java/itchetumal/edu/mx/dapps/reservaavion/MainActivity.java
@@ -0,0 +1,13 @@
+package itchetumal.edu.mx.dapps.reservaavion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
